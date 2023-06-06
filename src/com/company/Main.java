fbaa55d703bed2b29a4dea15e9e4020968a2b75a
arunkudiyal <arunkudiyal@icloud.com>
2023-06-06 16:05:07 +0530

    Introduction to DSA & Introduction to Java - Basics

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+// Java is an OOP.
+// package --> A collection of java class
+// Default Package -> java.lang
+class Main {
+    public static void main(String[] args) {
+        // Display a message :- ( cout << "" , printf("") )
+        System.out.println("Hello World!");
+        System.out.print("Hello...World...");
+    }
+}
