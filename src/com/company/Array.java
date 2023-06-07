68b35fcc06d82c51d4fcb7d378d07c2252fb642c
arunkudiyal <arunkudiyal@icloud.com>
2023-06-07 16:12:19 +0530

    Array Basics + Problems on Array

---
+++ src/com/company/Array.java
@@ -0,0 +1,34 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Array {
+    public static void main(String[] args) {
+        // Arrays :- It is a linear DSA, which allows you to contain multiple values in a single variable
+        // 1. Arrays in java is uni-set (of a single data type)
+        // 2. Arrays can help you store values, but you need to access them using indices (starts with 0)
+
+        // Syntax -> datatype[] array_name = { a, b, c, d, ... };
+         byte[] arr = {10, 15, 20, 30, 45, 100};
+         // index -> 0, 1, 2, 3, 4, 5
+        System.out.println(arr[0] + " " + arr[1] + " " + arr[2] + " " + arr[3] + " " + arr[4] + " " + arr[5]);
+        System.out.println(arr.length);
+
+        // Syntax -> when you have the size of the array, but not the values.
+        // datatype[] array_name = new datatype[size];
+        // int[] array = new int[5];
+
+        // Q:- Given a size from the user, take the input from the user and display the values.
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();                // 5
+        int[] array = new int[size];
+        // taking the array input
+        for(int i=0; i < size; i++)             // i = 0, 1, 2, 3, 4
+            // array[0] = sc.nextInt(), array[1] = sc.nextInt()
+            array[i] = sc.nextInt();
+        // display
+        for(int i=0; i < size; i++)
+            System.out.print(array[i] + " ");
+        System.out.println();
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.util.Scanner;
+
 // Java is an OOP.
 // package --> A collection of java class
 // Default Package -> java.lang
@@ -8,5 +10,27 @@ class Main {
         // Display a message :- ( cout << "" , printf("") )
         System.out.println("Hello World!");
         System.out.print("Hello...World...");
+        System.out.println();
+
+        // Primitive Data Types (PDTs) --> Statically Typed Language
+        byte one = -128; // size -> 1 byte, range -> -128 to +127
+        short two = 32767; // size -> 2 bytes, range -> -32768 to +32767
+        int three = 40000; // size -> 4 bytes, rage -> -2^31 to 2^31 - 1
+        float four = 3.000000000000000000000000014f;        // 7 decimal places
+        double five = 3.000000000000000000000000014;        // 13 decimal places
+        boolean six = true; // size - 1 byte, range -> T/F
+        long seven = 1000000078; // size -> 8 bytes, rage -> -2^62 to 2^62 - 1
+
+        // Input or Output
+        // Input --> 1. Scanner Class --> a.) import the class, b.) create an object, 3.) taking the ip from user
+        // Syntax -> Scanner objectName = new Scanner(System.in)
+
+        // Research --> How to take a char and string as an input from the Scanner class.
+        // IMP :- Scanner has two delimiters --> space and enter
+        Scanner sc = new Scanner(System.in);
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        boolean isOk = sc.nextBoolean();
+        System.out.println(num1 + num2);
     }
 }
