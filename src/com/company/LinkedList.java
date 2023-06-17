941c34ca85c350be61b69d2bceb53efe8de77a00
arunkudiyal <arunkudiyal@icloud.com>
2023-06-17 15:48:11 +0530

    Utility Function - CRUD - insertAtEnd(), insertAfter(), deleteAtEnd(), deleteFromStart()

--- src/com/company/LinkedList.java
+++ src/com/company/LinkedList.java
@@ -27,6 +27,46 @@ public class LinkedList {
             temp.next = newNode;
         }
     }
+    // Utility function which inserts the node at the start of the LL
+    public void insertAtBeg(int data) {
+        Node newNode = new Node(data);
+        if(head == null) head = newNode;
+        else {
+            Node temp = head;
+            head = newNode;
+            newNode.next = temp;
+        }
+    }
+    // Utility function which inserts data1 after data2
+    public void insertAfter(int data1, int data2) {
+        Node temp = head;
+        // a.) check if d2 exists in the LL or not
+        while(temp.data != data2) {
+            temp = temp.next;
+            if(temp == null) {
+                System.out.println(data2 + " is not available in the LL, use insertEnd() to insert the data!");
+                break;
+            }
+        }
+        // b.) d2 exists in the LL (temp.data == data)
+        if(temp != null) {
+            Node newNode = new Node(data1);
+            Node temp2 = temp.next;
+            temp.next = newNode;
+            newNode.next = temp2;
+        }
+    }
+    // Utility function to remove the last element of the LinkedList
+    public void deleteFromEnd() {
+        Node temp = head;
+        while(temp.next.next != null) temp = temp.next;
+        temp.next = null;
+    }
+    // Utility function to remove the first element from the LinkedList
+    public void deleteFromStart() {
+        head = head.next;
+    }
+    // Utility function to print the values of the LL in the forward direction
     public void display() {
         Node temp = head;
         while(temp != null) {
@@ -44,5 +84,13 @@ public class LinkedList {
         list.insertAtEnd(400);
         list.insertAtEnd(500);
         list.display();                                     // 100 200 300 400 500
+        list.insertAtBeg(10);
+        list.display();                                     // 10 100 200 300 400 500
+        list.insertAfter(50, 300);
+        list.display();                                     // 10 100 200 300 50 400 500
+        list.deleteFromEnd();
+        list.display();                                     // 10 100 200 300 50 400
+        list.deleteFromStart();
+        list.display();                                     // 100 200 300 50 400
     }
 }
