409be2b064204b95d4c0eb39b646571fa00c3680
arunkudiyal <arunkudiyal@icloud.com>
2023-06-22 15:05:23 +0530

    Orders - preOrder & postOrder

--- src/com/company/BinarySearchTree.java
+++ src/com/company/BinarySearchTree.java
@@ -21,6 +21,14 @@ public class BinarySearchTree {
         inOrderTraversal(root);
         System.out.println();
     }
+    public void preOrder() {
+        preOrderTraversal(root);
+        System.out.println();
+    }
+    public void postOrder() {
+        postOrderTraversal(root);
+        System.out.println();
+    }
     private Node insertData(Node root, int data) {
         if(root == null) root = new Node(data);
         else if(root.data > data) root.left = insertData(root.left, data);
@@ -34,6 +42,20 @@ public class BinarySearchTree {
             inOrderTraversal(root.right);       // R
         }
     }
+    private void preOrderTraversal(Node root) {
+        if(root != null) {
+            System.out.print(root.data + " ");  // D
+            preOrderTraversal(root.left);        // L
+            preOrderTraversal(root.right);       // R
+        }
+    }
+    private void postOrderTraversal(Node root) {
+        if(root != null) {
+            postOrderTraversal(root.left);      // L
+            postOrderTraversal(root.right);     // R
+            System.out.print(root.data + " ");        // D
+        }
+    }
     public static void main(String[] args) {
         BinarySearchTree bst = new BinarySearchTree();          // root = null;
         bst.insert(45);
@@ -44,7 +66,12 @@ public class BinarySearchTree {
         bst.insert(23);
         bst.insert(21);
         bst.insert(43);
+        bst.insert(1);
         // NOTE :- The inOrder of any BSt will always be in ascending order!
-        bst.inOrder();
+        bst.inOrder();                                                              // 1 6 12 21 23 43 45 78 90
+        // NOTE :- The first element in the preOrder will always be root.
+        bst.preOrder();                                                             // 45 6 1 12 23 21 43 78 90
+        // NOTE :- The last element in the postOrder will always be root.
+        bst.postOrder();                                                            // 1 21 43 23 12 6 90 78 45
     }
 }
