1a9b64c96680b0d6954606ab0af94697c30d58f8
arunkudiyal <arunkudiyal@icloud.com>
2023-07-08 09:52:49 +0530

    Matrix Chain Multiplication - COMPLETED

--- src/com/company/MatrixChainMultiplication.java
+++ src/com/company/MatrixChainMultiplication.java
@@ -3,20 +3,27 @@ package com.company;
 import java.util.Scanner;
 
 public class MatrixChainMultiplication {
-    public int matrixChainMultiplication(int n, int[] arr) {
-        int m = n-1;
-        int[][] table = new int[m][m];
-        for(int length = 2; length <= m; length++) {
-            for(int i=0; i < m-length+1; i++) {
-                int j = i + length - 1;
-                table[i][j] = Integer.MAX_VALUE;
-                for(int k=i; k < j; k++) {
-                    int cost = table[i][k] + table[k+1][j] + (arr[i] * arr[j+1] * arr[k+1]);
-                    if(cost < table[i][j]) table[i][j] = cost;
-                }
+    int[][] memo;
+    public int matrixChainMultiplicationUtil(int[] dimensions, int i, int j) {
+        if(i == j) return 0;
+        if(memo[i][j] != -1) return memo[i][j];
+        memo[i][j] = Integer.MAX_VALUE;
+        for(int k=i; k < j; k++) {
+            int cost = matrixChainMultiplicationUtil(dimensions, i, k) + matrixChainMultiplicationUtil(dimensions, k+1, j) + (dimensions[i-1] * dimensions[k] * dimensions[j]);
+            memo[i][j] = Math.min(memo[i][j], cost);
+        }
+        return memo[i][j];
+    }
+    public int matrixChainMultiplication(int[] arr) {
+        int n = arr.length;
+        memo = new int[n][n];
+        for(int i=0; i < n; i++) {
+            for(int j=0; j < n; j++) {
+                // -1 represents that none of the values is calculated
+                memo[i][j] = -1;
             }
         }
-        return table[1][n-1];
+        return matrixChainMultiplicationUtil(arr, 1, n-1);
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -25,7 +32,7 @@ public class MatrixChainMultiplication {
         for(int i=0; i < n; i++)
             arr[i] = sc.nextInt();
         MatrixChainMultiplication mcm = new MatrixChainMultiplication();
-        int answer = mcm.matrixChainMultiplication(n, arr);
+        int answer = mcm.matrixChainMultiplication(arr);
         System.out.println(answer);
     }
 }
