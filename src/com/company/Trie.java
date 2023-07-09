a59c10d4037eef3689a70d15015878c5674fd181
arunkudiyal <arunkudiyal@icloud.com>
2023-07-09 15:50:20 +0530

    Tries - insert() & search()

--- src/com/company/Trie.java
+++ src/com/company/Trie.java
@@ -2,31 +2,56 @@ package com.company;
 
 import java.util.Scanner;
 
-public class Trie {
-    static Node root;
-    Trie() {}
-    static class Node {
-        Node[] children = new Node[26];
-        boolean eow;
-        Node() {
-            this.eow = false;
-            for(int i=0; i < 26; i++)
-                children[i] = null;
-        }
+class Node {
+    Node[] children = new Node[26];
+    boolean eow;
+    Node() {
+        for(int i=0; i < 26; i++)
+            this.children[i] = null;
+        this.eow = false;
     }
-    public static void insert(String word) {
-
+}
+class Trie {
+    Node root;
+    Trie() {
+        root = new Node();
+    }
+    public void insert(String word) {
+        Node temp = root;
+        for(int i=0; i < word.length(); i++) {
+            char ch = word.charAt(i);
+            int index = ch - 'a';
+            if(temp.children[index] == null)
+                temp.children[index] = new Node();
+            temp = temp.children[index];
+        }
+        temp.eow = true;
+    }
+    public boolean search(String key) {
+        int index;
+        int length = key.length();
+        Node temp = root;
+        for(int level=0; level < length; level++) {
+            index = key.charAt(level) - 'a';
+            if(temp.children[index] == null) return false;
+            temp = temp.children[index];
+        }
+        return temp.eow;
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        // total number of words
+        // no of words in the S
         int n = sc.nextInt();
-        // set 'S' contains all the words in the Set
         String[] S = new String[n];
         for(int i=0; i < n; i++)
-            S[i] = sc.nextLine();
-        String P = sc.nextLine();
+            S[i] = sc.next();
+        Trie trie = new Trie();
         for(int i=0; i < n; i++)
-            insert(S[i]);
+            trie.insert(S[i]);
+        // Value to be matched
+        String toBeMatched = sc.next();
+        boolean ans = trie.search(toBeMatched);
+        if(ans) System.out.println(toBeMatched + " is available in S");
+        else System.out.println(toBeMatched + " is not available in S");
     }
 }
